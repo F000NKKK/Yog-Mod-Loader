aa6bf5b6b70d3a1e874f41db344bc23d160cf72e
F000NKKK <burtelgamerpro@gmail.com>
2026-07-13 23:07:49 +0600

    feat(forge): add dimension parameter to native event handlers
    Add the player's current dimension location (e.g., "minecraft:overworld")
    to the native bridge callbacks for advancement, block placement, container
    open/close, crafting, and item pickup events.

--- forge/platforms/1.21.1/src/main/java/dev/yog/mixin/BlockItemPlaceMixin.java
+++ forge/platforms/1.21.1/src/main/java/dev/yog/mixin/BlockItemPlaceMixin.java
@@ -29,6 +29,7 @@ public class BlockItemPlaceMixin {
         BlockPos pos = context.getClickedPos();
         String blockId = BuiltInRegistries.BLOCK.getKey(((BlockItem) (Object) this).getBlock()).toString();
         NativeBridge.nativeOnPlaceBlock(
-                sp.getName().getString(), blockId, pos.getX(), pos.getY(), pos.getZ());
+                sp.getName().getString(), blockId, pos.getX(), pos.getY(), pos.getZ(),
+                context.getLevel().dimension().location().toString());
     }
 }
